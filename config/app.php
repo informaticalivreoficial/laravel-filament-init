da184739dc10814ed47bf08ca0e27a2d347a32fe
renato-Aspire-E5-574 <renatomontanari@ymail.com>
2023-10-14 10:09:13 -0300

    update laravel 8 for 9

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -231,7 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Goutte' => Weidner\Goutte\GoutteFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
 
